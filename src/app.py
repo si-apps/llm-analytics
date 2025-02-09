e5382c0e51b34721110a706095554130ddbc3af3
orin <orin@imperva.com>
2025-02-09 14:21:51 +0200

    update version

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ app = Flask(__name__, static_url_path="", static_folder="static")
 Compress(app)
 visitors_limit = VisitorsLimit(200, 20, 3600)
 
-_VERSION = "0.0.5"
+_VERSION = "0.0.6"
 
 
 @app.route('/ping')
